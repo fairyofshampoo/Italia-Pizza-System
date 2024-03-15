5c42efa3529b32fdee40a442793d5ad6201eb559
itsue <sujey542003@gmail.com>
2024-03-15 09:59:39 -0600

    Métodos DAO de Empleado

---
+++ ItaliaPizza/ItaliaPizza/DataLayer/DAO/Interface/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItaliaPizza.DataLayer.DAO.Interface
+{
+    internal interface IEmployee
+    {
+        bool AddEmployee(Employee employee, Account acount);
+        bool IsEmailExisting(string  email);
+        bool IsUserExisting(String user);
+    }
+}
