681592f9b02a54de56a2a9c3b0f690d68178f420
Star3oy <115896405+Star3oy@users.noreply.github.com>
2024-03-16 14:56:41 -0600

    Todo lo que he trabajado

---
+++ ItaliaPizza/ItaliaPizza/DataLayer/DAO/AddressDAO.cs
@@ -0,0 +1,21 @@
+﻿using ItaliaPizza.DataLayer.DAO.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItaliaPizza.DataLayer.DAO
+{
+    internal class AddressDAO : IAddress
+    {
+        public Address GetClientAddress(string email)
+        {
+            using (var databaseContext = new ItaliaPizzaDBEntities())
+            {
+                var address = databaseContext.Addresses.FirstOrDefault(addressDB => addressDB.clientId == email);
+                return address;
+            }
+        }
+    }
+}
