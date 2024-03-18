817f9eaa263a911b883bb8f067803bffc55de49f
Star3oy <115896405+Star3oy@users.noreply.github.com>
2024-03-17 19:48:44 -0600

    Añadí la pantalla de editar cliente

---
+++ ItaliaPizza/ItaliaPizza/UserInterfaceLayer/UsersModule/EditClientView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizza.UserInterfaceLayer.UsersModule
+{
+    public partial class EditClientView : Page
+    {
+        public EditClientView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisable_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
