ccb647f612b7e86191cb8ae9dca22dc21574cdbd
itsue <sujey542003@gmail.com>
2024-03-24 00:14:33 -0600

    Diseño de ventana de productos y tarjeta

---
+++ ItaliaPizza/ItaliaPizza/UserInterfaceLayer/ProductsModule/ProductsView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizza.UserInterfaceLayer.ProductsModule
+{
+    /// <summary>
+    /// Lógica de interacción para ProductsView.xaml
+    /// </summary>
+    public partial class ProductsView : Page
+    {
+        public ProductsView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAllFilter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInternalFilter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExternalFilter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInventario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
