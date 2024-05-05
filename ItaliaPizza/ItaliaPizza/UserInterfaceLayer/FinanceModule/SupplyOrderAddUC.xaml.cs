7f3270825a6493a4d33c56c5162f7fe8e1449a31
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-05-05 11:16:06 -0600

    Creacion de UC para Orden de Proveedor

---
+++ ItaliaPizza/ItaliaPizza/UserInterfaceLayer/FinanceModule/SupplyOrderAddUC.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ItaliaPizza.DataLayer;
+using ItaliaPizza.DataLayer.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizza.UserInterfaceLayer.FinanceModule
+{
+    /// <summary>
+    /// Interaction logic for SupplyOrderAddUC.xaml
+    /// </summary>
+    public partial class SupplyOrderAddUC : UserControl
+    {
+        private Supply SupplyData;
+        public SupplyOrderAddUC()
+        {
+            InitializeComponent();
+        }
+        public void SetTitleData()
+        {
+            txtAmount.FontWeight = FontWeights.Bold;
+            txtName.FontWeight = FontWeights.Bold;
+            txtSupplyArea.FontWeight = FontWeights.Bold;
+            txtUnit.FontWeight = FontWeights.Bold;
+            btnAddSupply.Visibility = Visibility.Hidden;
+        }
+        public void SetProductData(Supply supply)
+        {
+            SupplyData = supply;
+            txtName.Text = supply.name;
+            txtAmount.Text = supply.amount.ToString();
+            txtSupplyArea.Text = supply.SupplyArea.area_name;
+            txtUnit.Text = supply.measurementUnit;
+        }
+    }
+}
---
+++ ItaliaPizza/ItaliaPizza/UserInterfaceLayer/FinanceModule/SupplyOrderView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizza.UserInterfaceLayer.FinanceModule
+{
+    /// <summary>
+    /// Interaction logic for SupplyOrder.xaml
+    /// </summary>
+    public partial class SupplyOrderView : Page
+    {
+        public SupplyOrderView()
+        {
+            InitializeComponent();
+            SetSuppliesInPage();
+        }
+
+        private void SetSuppliesInPage()
+        {
+            //Consultar supplies activos con método GetSuppliesAvailable();
+            SupplyOrderAddUC supplyUC = new SupplyOrderAddUC();
+            supplyUC.SetTitleData();
+            suppliesListView.Items.Add(supplyUC);
+            //Por cada supply activo hay que mandar a crear un SupplyOrderAddUC
+        }
+
+        private void BtnSaveOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
